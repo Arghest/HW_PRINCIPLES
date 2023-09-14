4934de8f1c7652632c13f769f7dc93ab3df4ad5b
Arghest <bastet.wilhelm0@gmail.com>
2023-09-15 00:35:11 +0700

    initialcommit1

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,9 +1,11 @@
 public class Post {
     public String name; // поле для имени
-    public String surname; //поле для фамилии
-    public String patronymic; //поле для отчества
+    public String surname;
+    public String patronymic;
     public String passport;
     public String phone;
     public boolean subscription;
+
+    public FormDate birthday = new FormDate();
 
 }
