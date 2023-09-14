557e0de0ebb0cd7b55cc5684ed9f680386f1c0da
Arghest <bastet.wilhelm0@gmail.com>
2023-09-15 00:33:00 +0700

    initialcommit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; //поле для фамилии
+    public String patronymic; //поле для отчества
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+}
